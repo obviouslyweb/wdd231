e9f6294e6e389527c019437c022abd8e082c6b2b
obviouslyweb <conbutter12@gmail.com>
2025-05-08 19:43:39 -0600

    Completed NPS 3.

--- src/js/main.js
+++ src/js/main.js
@@ -2,25 +2,31 @@ import { getParkData, getInfoLinks } from "./parkService.mjs";
 import { mediaCardTemplate } from "./templates.mjs";
 import { setHeaderFooter } from "./setHeaderFooter.mjs";
 
-const parkData = getParkData();
-const parkInfoLinks = getInfoLinks();
+async function init() {
+    const parkData = await getParkData();
+    const links = getInfoLinks(parkData.images);
 
-function setMainInfo(data) {
+    setHeaderFooter(parkData);
+    setParkIntro(parkData);
+    setParkInfoLinks(links);
+}
+
+function setParkIntro(data) {
     // Update park name & description
     const main_title = document.querySelector(".park-name");
     const main_description = document.querySelector(".park-description");
-    main_title.innerHTML = parkData.fullName;
-    main_description.innerHTML = parkData.description;
+    main_title.innerHTML = data.fullName;
+    main_description.innerHTML = data.description;
+    createInfoCards(data.images);
 }
 
-function createInfoCards() {
+function createInfoCards(images) {
     const cardHolder = document.querySelector('.park-info-card-holder');
     cardHolder.innerHTML = '';
-    parkInfoLinks.forEach(info =>{
+    const infoLinks = getInfoLinks(images);
+    infoLinks.forEach(info => {
         cardHolder.innerHTML += mediaCardTemplate(info);
     });
 }
 
-setMainInfo(parkData);
-createInfoCards();
-setHeaderFooter(parkData);
+init();
--- src/js/parkService.mjs
+++ src/js/parkService.mjs
@@ -1,3 +1,7 @@
+const baseUrl = "https://developer.nps.gov/api/v1/";
+const apiKey = import.meta.env.VITE_NPS_API_KEY;
+// "baseUrl + resourcePath + parameters" for API calls
+
 const park = {
   id: "F58C6D24-8D10-4573-9826-65D42B8B83AD",
   url: "https://www.nps.gov/yell/index.htm",
@@ -179,8 +183,24 @@ const park = {
   designation: "National Park"
 };
 
-export function getParkData() {
-  return park;
+export async function getParkData() {
+  const parkData = await getJson("parks?parkCode=yell");
+  return parkData.data[0];
+}
+
+async function getJson(url) {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-Api-Key": apiKey
+    }
+  };
+  let data = {};
+  const response = await fetch(baseUrl + url, options);
+  if (response.ok) {
+    data = await response.json();
+  } else throw new Error("Response not ok");
+  return data;
 }
 
 const parkInfoLinks = [
@@ -205,6 +225,11 @@ const parkInfoLinks = [
   }
 ];
 
-export function getInfoLinks() {
-  return parkInfoLinks;
+export function getInfoLinks(data) {
+    // Why index + 2 below? no real reason. we don't want index 0 since that is the one we used for the banner...I decided to skip an image.
+  const withUpdatedImages = parkInfoLinks.map((item, index) => {
+    item.image = data[index + 2].url;
+    return item;
+  });
+  return withUpdatedImages;
 }
