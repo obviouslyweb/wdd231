68070a1876cb71e1646f2cc2db360fb67b687109
obviouslyweb <conbutter12@gmail.com>
2025-06-09 15:54:48 -0600

    Updated functionality.

--- src/week7/pride.js
+++ src/week7/pride.js
@@ -16,31 +16,38 @@ async function getQuotes() {
 
         quotes = json.books[0].quotes;
 
-        quotes.forEach((element) => {
-            list_item = document.createElement("li");
-            list_item.textContent = element;
-            quote_list.appendChild(list_item);
-        });
+        inputQuotes(quotes);
         
-        localStorage.setItem("pride-prejudice", quotes);
-
-        quote_button.classList.add("hidden");
+        localStorage.setItem("pride-prejudice", JSON.stringify(quotes));
 
     } catch (error) {
             console.error(error.message);
     }
 }
 
-quote_button.addEventListener("click", getQuotes);
+function inputQuotes(quotes) {
+    quotes.forEach((element) => {
+        list_item = document.createElement("li");
+        list_item.textContent = element;
+        quote_list.appendChild(list_item);
+    });
+    quote_button.classList.add("hidden");
+}
 
-loadFromLocalStorage();
+function checkQuotes() {
+    const storedQuotes = localStorage.getItem("pride-prejudice");
+    if (storedQuotes) {
+        quote_list.removeChild(no_quotes_remind);
+        quotes = JSON.parse(storedQuotes);
+        inputQuotes(quotes);
+    }
+}
+
+quote_button.addEventListener("click", getQuotes);
+checkQuotes();
 
 // Once checked in, your site should be accessible at https://<your-netlify-name-here>.netlify.app/week7/pride.html
 
 // Style the button so that it uses an animation to “pulse” (grow and shrink) the button.
-
-// Add code so that if the quotes are already present in local storage, the list is displayed from local storage and no API call is made.
-
-// Also, if the quotes list is already present, hide the button when the page is loaded
 
 // Add some style to the page by updating the font, font-size, background colors, button color, padding, margin and text alignment as you see fit.s
